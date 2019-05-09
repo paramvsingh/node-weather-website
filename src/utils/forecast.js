21022d70b38ffd107027ae0efe78597f60a8b8be
Paramvir Singh <paramvir.singh@ituple.com>
2019-05-09 14:56:28 +0530

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -23,7 +23,7 @@ const forecast=(latitude,longitude,callback)=>{
       callback(`${body.error}`,undefined)
     }
     else{
-      callback(undefined,`${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability*100}% chance of rain.`)
+      callback(undefined,`${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. The high today is ${body.daily.data[0].temperatureHigh} with a low of ${body.daily.data[0].temperatureLow}. There is a ${body.currently.precipProbability*100}% chance of rain.`)
     }
   })
 }
